f6791fdb6016194b189d57deea66bc2b024167d2
Thayyyyyyyibbbbbbbb <thabis24@kingshigh.school.nz>
2026-06-08 14:55:31 +1200

    V1
    Added tiles

--- new-game-project/animated_sprite_2d.gd
+++ new-game-project/animated_sprite_2d.gd
@@ -2,15 +2,4 @@ extends AnimatedSprite2D
 
 func _process(delta):
 	# Check if the right arrow key is pressed
-	if Input.is_action_pressed("ui_right"):
-		play("run")
-		flip_h = false # Faces the sprite to the right
-		
-	# Check if the left arrow key is pressed
-	elif Input.is_action_pressed("ui_left"):
-		play("run")
-		flip_h = true  # Flips the sprite to face left
-		
-	# If no keys are pressed, play the idle animation
-	else:
-		play("idle")
+	
